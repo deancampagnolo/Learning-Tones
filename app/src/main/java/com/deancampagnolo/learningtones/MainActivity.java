26512d040c5b2243d4ecbb4b0b624e16ea807b95
Dean Campagnolo <dcampagn@ucsc.edu>
2018-04-18 00:13:39 -0700

    Fixed the bug that made the application not launch (declared a variable too early) Fixed the bug the made the selector screen options not list the actual values

--- app/src/main/java/com/deancampagnolo/learningtones/MainActivity.java
+++ app/src/main/java/com/deancampagnolo/learningtones/MainActivity.java
@@ -1,13 +1,31 @@
 package com.deancampagnolo.learningtones;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button nameThatToneButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nameThatToneButton = (Button)findViewById(R.id.nameThatToneButton);
+        nameThatToneButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, NameThatToneSelectorScreen.class));
+            }
+        });
+
     }
+
+
+    /*public void onClick(View v){
+
+    }*/
 }
