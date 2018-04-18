a422d358f1b10dbe0479d2ff052ffd743b5a756a
Dean Campagnolo <dcampagn@ucsc.edu>
2018-04-18 14:29:39 -0700

    Trying to get the buttons and Log to work.

--- app/src/main/java/com/deancampagnolo/learningtones/MainActivity.java
+++ app/src/main/java/com/deancampagnolo/learningtones/MainActivity.java
@@ -3,15 +3,18 @@ package com.deancampagnolo.learningtones;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     Button nameThatToneButton;
+    String TAG = "dean";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.v(TAG, "Weemp Wompz");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         nameThatToneButton = (Button)findViewById(R.id.nameThatToneButton);
--- app/src/main/java/com/deancampagnolo/learningtones/NameThatToneSelectorScreen.java
+++ app/src/main/java/com/deancampagnolo/learningtones/NameThatToneSelectorScreen.java
@@ -2,12 +2,45 @@ package com.deancampagnolo.learningtones;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+
 
 public class NameThatToneSelectorScreen extends AppCompatActivity {
+
+    private CheckBox a, as, b, c, cs, d, ds, e, f, fs, g, gs;// declared for every checkbox/note
+    String TAG = "Dean";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name_that_tone_selector_screen);
+
+        Log.v(TAG, "Weemp Womp");
+
+
+        a = (CheckBox)findViewById(R.id.aCheck);
+        as = (CheckBox)findViewById(R.id.asCheck);
+        b = (CheckBox)findViewById(R.id.bCheck);
+        c = (CheckBox)findViewById(R.id.cCheck);
+        cs = (CheckBox)findViewById(R.id.csCheck);
+        d = (CheckBox)findViewById(R.id.dCheck);
+        ds = (CheckBox)findViewById(R.id.dsCheck);
+        e = (CheckBox)findViewById(R.id.eCheck);
+        f = (CheckBox)findViewById(R.id.fCheck);
+        g = (CheckBox)findViewById(R.id.gCheck);
+        gs = (CheckBox)findViewById(R.id.gsCheck);
+
     }
+
+    public void onCheckboxClicked(View v){
+        boolean checked = ((CheckBox)v).isChecked();
+        switch(v.getId()){
+            case R.id.aCheck:
+                Log.v(TAG, "asdljldsakfjlsdkfjlsdkj");
+        }
+    }
+
+
 }
