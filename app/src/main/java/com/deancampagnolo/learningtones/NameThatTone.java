0b1e44f4327839f898c89790f1d908adc23fc8c8
Dean Campagnolo <dcampagn@ucsc.edu>
2018-04-19 18:24:18 -0700

    Changed the arraylist in selector screen back to an array. Now I am passing a boolean array to name that tone instead of an array of checkboxes.

--- app/src/main/java/com/deancampagnolo/learningtones/NameThatTone.java
+++ app/src/main/java/com/deancampagnolo/learningtones/NameThatTone.java
@@ -1,15 +1,30 @@
 package com.deancampagnolo.learningtones;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.CheckBox;
 
 import java.util.ArrayList;
 
 public class NameThatTone extends AppCompatActivity {
-    ArrayList<CheckBox> theNotes;
+    private final int NUMBEROFNOTES = 12;
+    private String TAG = "dean";
+    private void p(String a){
+        Log.v(TAG, a);
+    }
+    //ArrayList<CheckBox> theNotes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Intent lastIntent = getIntent();
+        //String yes = lastIntent.getStringExtra("yes");
+        //p(yes);
+        boolean[] notesBoolean = lastIntent.getBooleanArrayExtra("notesBoolean");
+        for(int i = 0; i<notesBoolean.length; i++){
+            p(""+notesBoolean[i]);
+        }
+        p("h");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name_that_tone);
 
