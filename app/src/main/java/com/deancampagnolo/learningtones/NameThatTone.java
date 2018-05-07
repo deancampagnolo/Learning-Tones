d902923b6827d5852dcc432d38a12a38d8664571
Dean Campagnolo <dcampagn@ucsc.edu>
2018-05-07 14:04:33 -0700

    added all the piano mp3 files in addition to changing Name that Tone by adding functions to create new array lists with the usable notes and usable sound pool  notes

--- app/src/main/java/com/deancampagnolo/learningtones/NameThatTone.java
+++ app/src/main/java/com/deancampagnolo/learningtones/NameThatTone.java
@@ -13,45 +13,87 @@ import android.widget.GridLayout;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 
 public class NameThatTone extends AppCompatActivity {
+    private int usableNotesStart = 1;
+    private int usableNotesFinish = 7;
     private final int NUMBEROFNOTES = 12;
     private String TAG = "dean";
     private boolean[] notesBoolean;
     private CheckBox[] allTheNotes;
     private GridLayout gridLayout;
-    private int[] usableNotes;
+    private ArrayList<Integer> usableNotes;
+    private ArrayList<Integer> soundPoolUsableNotes;
+    private int currentNote;
+    private int numberOfNotesBeingUsed;
+    private SoundPool sp;
 
     //This function helps with debugging by logging values
     private void p(String a){
         Log.v(TAG, a);
     }
 
-    /*private void createUsableNotes(){
+    private void createUsableNotes(){
+        usableNotes = new ArrayList<>();
         for(int i = 0; i<notesBoolean.length; i++){
             if(notesBoolean[i]){//if it is true
+
                 switch(i){
-                    case 0:
-
-                }
-
-
-
-
-            }
-        }
-
+                    case 0://case a
+                        addingIntoUsableNotes(usableNotesStart,usableNotesFinish,"a");
+                        break;
+                    case 1://case bb
+                        addingIntoUsableNotes(usableNotesStart,usableNotesFinish,"bb");
+                        break;
+                    case 2://case b
+                        addingIntoUsableNotes(usableNotesStart,usableNotesFinish,"b");
+                        break;
+                    case 3://case c
+                        addingIntoUsableNotes(usableNotesStart,usableNotesFinish,"c");
+                        break;
+                    case 4://case db
+                        addingIntoUsableNotes(usableNotesStart,usableNotesFinish,"db");
+                        break;
+                    case 5://case d
+                        addingIntoUsableNotes(usableNotesStart,usableNotesFinish,"d");
+                        break;
+                    case 6://case eb
+                        addingIntoUsableNotes(usableNotesStart,usableNotesFinish,"eb");
+                        break;
+                    case 7://case e
+                        addingIntoUsableNotes(usableNotesStart,usableNotesFinish,"e");
+                        break;
+                    case 8://case f
+                        addingIntoUsableNotes(usableNotesStart,usableNotesFinish,"f");
+                        break;
+                    case 9://case gb
+                        addingIntoUsableNotes(usableNotesStart,usableNotesFinish,"gb");
+                        break;
+                    case 10://case g
+                        addingIntoUsableNotes(usableNotesStart,usableNotesFinish,"g");
+                        break;
+                    case 11://case ab
+                        addingIntoUsableNotes(usableNotesStart,usableNotesFinish,"ab");
+                        break;
+                }//end of switch
+            }//end of if
+        }//end of for
     }
 
-    private void addingIntoUsableNotes(int start, int finish, char note){
-
-        int id = getApplicationContext().getResources().getIdentifier("A0.wav","raw",getPackageName());
-        p(""+id);
-        p(""+R.raw.a0);
+    private void addingIntoUsableNotes(int start, int finish, String note){
         for(int i = start; i<=finish;i++){
+            usableNotes.add(getApplicationContext().getResources().getIdentifier(note + i,"raw",getPackageName()));
         }
-    }*/
+    }
+
+    private void loadRealNotes(SoundPool sp){
+        soundPoolUsableNotes = new ArrayList<>();
+        for(int i = 0;i<usableNotes.size();i++){
+            soundPoolUsableNotes.add(sp.load(this,usableNotes.get(i),1));
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,14 +111,18 @@ public class NameThatTone extends AppCompatActivity {
         //R.id.
         initializeNotes();
         unnecessaryNotesDisappear();
-        //int id = getApplicationContext().getResources().getIdentifier("A0.wav","raw",getPackageName());
+        //int id = getApplicationContext().getResources().getIdentifier("a0","raw",getPackageName());
         //p("FIRST"+id);
         //p("SECOND"+R.raw.a0);
+        createUsableNotes();
+        sp = new SoundPool.Builder().setMaxStreams(20).build();//samples default to 1 which I want.
+        loadRealNotes(sp);
 
-        //createUsableNotes();
 
-        SoundPool sp = new SoundPool.Builder().setMaxStreams(5).build();//samples default to 1 which I want.
-        //int k = sp.load(this,R.raw.a0,1);
+
+        int firstNote = (int)(Math.random()*1);
+
+
 
 
         //traverse(R.raw);
@@ -85,7 +131,7 @@ public class NameThatTone extends AppCompatActivity {
     }
 
 
-    public void traverse (File dir) {
+    /*public void traverse (File dir) {
         if (dir.exists()) {
             File[] files = dir.listFiles();
             for (int i = 0; i < files.length; ++i) {
@@ -97,7 +143,7 @@ public class NameThatTone extends AppCompatActivity {
                 }
             }
         }
-    }
+    }*/
 
     //This function initializes the CheckBoxes in nameThatTone into the allTheNotes CheckBox array
     public void initializeNotes(){
@@ -128,6 +174,7 @@ public class NameThatTone extends AppCompatActivity {
         switch(v.getId()){
             case R.id.hearAgainButton:
                 p("hearAgainButton pressed");
+                sp.play(soundPoolUsableNotes.get(5),1,1,1,1,.5f);
                 break;
 
             case R.id.submitButton:
