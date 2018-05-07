a158349634c15ea5a427f23166164b5e402e38e9
Dean Campagnolo <dcampagn@ucsc.edu>
2018-05-06 17:15:44 -0700

    only the a-ab and c1 files allow the gradle to compile

--- app/src/main/java/com/deancampagnolo/learningtones/NameThatTone.java
+++ app/src/main/java/com/deancampagnolo/learningtones/NameThatTone.java
@@ -2,7 +2,6 @@ package com.deancampagnolo.learningtones;
 
 
 import android.content.Intent;
-import android.media.AudioManager;
 import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,7 +10,9 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.GridLayout;
 
-
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 
 public class NameThatTone extends AppCompatActivity {
@@ -20,12 +21,37 @@ public class NameThatTone extends AppCompatActivity {
     private boolean[] notesBoolean;
     private CheckBox[] allTheNotes;
     private GridLayout gridLayout;
+    private int[] usableNotes;
 
     //This function helps with debugging by logging values
     private void p(String a){
         Log.v(TAG, a);
     }
 
+    /*private void createUsableNotes(){
+        for(int i = 0; i<notesBoolean.length; i++){
+            if(notesBoolean[i]){//if it is true
+                switch(i){
+                    case 0:
+
+                }
+
+
+
+
+            }
+        }
+
+    }
+
+    private void addingIntoUsableNotes(int start, int finish, char note){
+
+        int id = getApplicationContext().getResources().getIdentifier("A0.wav","raw",getPackageName());
+        p(""+id);
+        p(""+R.raw.a0);
+        for(int i = start; i<=finish;i++){
+        }
+    }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,12 +66,37 @@ public class NameThatTone extends AppCompatActivity {
 
         gridLayout = (GridLayout) findViewById(R.id.NTTGridLayout);
         allTheNotes = new CheckBox[NUMBEROFNOTES];
-
+        //R.id.
         initializeNotes();
         unnecessaryNotesDisappear();
-        SoundPool sp = new SoundPool.Builder().build();//samples default to 1 which I want.
-        int k = sp.load(this,R.raw.piano51,1);
-        sp.play(k,1,1,1,1,.5f);
+        //int id = getApplicationContext().getResources().getIdentifier("A0.wav","raw",getPackageName());
+        //p("FIRST"+id);
+        //p("SECOND"+R.raw.a0);
+
+        //createUsableNotes();
+
+        SoundPool sp = new SoundPool.Builder().setMaxStreams(5).build();//samples default to 1 which I want.
+        //int k = sp.load(this,R.raw.a0,1);
+
+
+        //traverse(R.raw);
+        //sp.play(k,1,1,1,1,.5f);
+        p("after play");
+    }
+
+
+    public void traverse (File dir) {
+        if (dir.exists()) {
+            File[] files = dir.listFiles();
+            for (int i = 0; i < files.length; ++i) {
+                File file = files[i];
+                if (file.isDirectory()) {
+                    traverse(file);
+                } else {
+                    // do something here with the file
+                }
+            }
+        }
     }
 
     //This function initializes the CheckBoxes in nameThatTone into the allTheNotes CheckBox array
